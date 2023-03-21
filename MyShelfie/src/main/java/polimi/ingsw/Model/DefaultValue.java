0ddf893dd2dc02b12a40700104283056a7700b27
Francesco Spangaro <francescospangaro@gmail.com>
2023-03-21 16:02:35 +0100

    Point calculator #2 Last touches, modified it so that it uses maps instead of arrays

--- MyShelfie/src/main/java/polimi/ingsw/Model/DefaultValue.java
+++ MyShelfie/src/main/java/polimi/ingsw/Model/DefaultValue.java
@@ -4,14 +4,15 @@ import java.util.Queue;
 
 public class DefaultValue {
 
-    public final static int MaxNumOfPlayer=4;
-    public final static int minNumOfPlayer=2;
+    public final static int MaxNumOfPlayer = 4;
+    public final static int minNumOfPlayer = 2;
 
-    public final static int NumOfCommonCards=2;
+    public final static int NumOfCommonCards = 2;
 
-    public final static int NumOfColumnsShelf =5;
-    public final static int NumOfRowsShelf =6;
+    public final static int NumOfColumnsShelf = 5;
+    public final static int NumOfRowsShelf = 6;
 
-    public static int pointsValue[] = {2,4,6,8};
+    public final static int NumOfTileTypes = 6;
+    public static int pointsValue[] = {2, 4, 6, 8};
 
 }
