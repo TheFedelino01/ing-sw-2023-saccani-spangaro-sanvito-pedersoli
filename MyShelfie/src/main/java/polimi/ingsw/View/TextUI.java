7ab355ce5bf2dd85bf5744efbcb16da4c1b8180b
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-04-04 15:25:30 +0200

    Update TextUI.java

--- MyShelfie/src/main/java/polimi/ingsw/View/TextUI.java
+++ MyShelfie/src/main/java/polimi/ingsw/View/TextUI.java
@@ -12,6 +12,7 @@ public class TextUI {
     private boolean isReadyToStart;
     private String answer;
 
+
     public TextUI() {
         nickname = "";
         gameID = 0;
@@ -21,41 +22,52 @@ public class TextUI {
 
     public static void main(String[] args) {
         TextUI textUI = new TextUI();
-        //textUI.insertNickname();
-        //textUI.selectGame();
-       // textUI.isPlayerReadyToStart();
-       // textUI.turn(true);
-        textUI.viewCommonCard(null);
+        textUI.insertNickname();
+        textUI.selectGame();
+        textUI.isPlayerReadyToStart();
+        textUI.turn(true);
     }
 
     public void insertNickname() {
-        System.out.println(this.hashCode() + "> Insert your nickname: ");
+        System.out.println("> Insert your nickname: ");
         nickname = scanner.nextLine();
-        System.out.println(this.hashCode() + "> Your nickname is: " + nickname);
+        System.out.println("> Your nickname is: " + nickname);
     }
 
     public void selectGame() {
-        System.out.println(this.hashCode() + "> Select the game you want to join: ");
-        gameID = Integer.parseInt(scanner.nextLine());
-        System.out.println(this.hashCode() + "> You have selected the game: " + gameID);
+        System.out.println("> Do you want to create a new game or join an existing one? (c/j)");
+        answer = scanner.nextLine();
+        if (answer.equals("c")) {
+            System.out.println("> You have selected to create a new game");
+            System.out.println("> Insert the number of players: ");
+            int numberOfPlayers = Integer.parseInt(scanner.nextLine());
+            System.out.println("> You have selected to create a new game with ID: " + gameID + " and " + numberOfPlayers + " players");
+        } else {
+            System.out.println("> You have selected to join an existing game");
+            System.out.println("> Select the game you want to join: ");
+            gameID = Integer.parseInt(scanner.nextLine());
+        }
+
+        System.out.println("> You have selected the game: " + gameID);
     }
 
     public void isPlayerReadyToStart() {
-        System.out.println(this.hashCode() + "> Are you ready to start? (y/n)");
-        answer = scanner.nextLine();
-        if (answer.equals("y"))
-            isReadyToStart = true;
-        else
-            isReadyToStart = false;
-
-        System.out.println(this.hashCode() + "> You have selected: " + isReadyToStart);
+        while(!isReadyToStart) {
+            System.out.println("> Are you ready to start? (y/n)");
+            answer = scanner.nextLine();
+            if (answer.equals("y"))
+                isReadyToStart = true;
+            else
+                isReadyToStart = false;
+        }
+        System.out.println("> You have selected: " + isReadyToStart);
     }
 
     public void turn(boolean isYourTurn) {
         if (isYourTurn)
-            System.out.println(this.hashCode() + "> It's your turn!");
+            System.out.println("> It's your turn!");
         else
-            System.out.println(this.hashCode() + "> It's not your turn!");
+            System.out.println("> It's not your turn!");
     }
 
     public void viewCommonCard(List<CommonCard> card) {
