5706f520d94acc5cc83d943b8e71a844d8711d0b
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-04-04 15:14:41 +0200

    Update TextUI.java

--- MyShelfie/src/main/java/polimi/ingsw/View/TextUI.java
+++ MyShelfie/src/main/java/polimi/ingsw/View/TextUI.java
@@ -1,13 +1,72 @@
 package polimi.ingsw.View;
 
+import polimi.ingsw.Model.Cards.Common.CommonCard;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class TextUI {
+    Scanner scanner = new Scanner(System.in);
     private String nickname;
-    public void insertNickname() {
-        System.out.println(this.hashCode()+"> Insert your nickname: ");
-        nickname = System.console().readLine();
+    private Integer gameID;
+    private boolean isReadyToStart;
+    private String answer;
+
+    public TextUI() {
+        nickname = "";
+        gameID = 0;
+        isReadyToStart = false;
+        answer = "";
     }
 
-    public void playerJoined(String nickNewPlayer) {
-        System.out.println(this.hashCode()+"> "+nickNewPlayer+" has just joined!");
+    public static void main(String[] args) {
+        TextUI textUI = new TextUI();
+        //textUI.insertNickname();
+        //textUI.selectGame();
+       // textUI.isPlayerReadyToStart();
+       // textUI.turn(true);
+        textUI.viewCommonCard(null);
+    }
+
+    public void insertNickname() {
+        System.out.println(this.hashCode() + "> Insert your nickname: ");
+        nickname = scanner.nextLine();
+        System.out.println(this.hashCode() + "> Your nickname is: " + nickname);
+    }
+
+    public void selectGame() {
+        System.out.println(this.hashCode() + "> Select the game you want to join: ");
+        gameID = Integer.parseInt(scanner.nextLine());
+        System.out.println(this.hashCode() + "> You have selected the game: " + gameID);
+    }
+
+    public void isPlayerReadyToStart() {
+        System.out.println(this.hashCode() + "> Are you ready to start? (y/n)");
+        answer = scanner.nextLine();
+        if (answer.equals("y"))
+            isReadyToStart = true;
+        else
+            isReadyToStart = false;
+
+        System.out.println(this.hashCode() + "> You have selected: " + isReadyToStart);
+    }
+
+    public void turn(boolean isYourTurn) {
+        if (isYourTurn)
+            System.out.println(this.hashCode() + "> It's your turn!");
+        else
+            System.out.println(this.hashCode() + "> It's not your turn!");
+    }
+
+    public void viewCommonCard(List<CommonCard> card) {
+    }
+
+    public void viewPlayGround() {
+    }
+
+    public void viewGoalCard() {
+    }
+
+    public void viewShelf() {
     }
 }
