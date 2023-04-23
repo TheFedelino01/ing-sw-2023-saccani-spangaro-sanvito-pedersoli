0570c5ca1e0b56bf8168bdab54578953fb82b78e
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-04-23 16:51:50 +0200

    Create PointTest.java

---
+++ MyShelfie/src/test/java/polimi/ingsw/Model/PointTest.java
@@ -0,0 +1,16 @@
+package polimi.ingsw.Model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PointTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    @DisplayName("Test player points")
+    void testPlayerPoints() {
+    }
+}
