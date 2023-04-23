60664f0ab4673ee6ec1380dafd93d647fac722fb
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-04-23 16:56:52 +0200

    Update PointTest.java

--- MyShelfie/src/test/java/polimi/ingsw/Model/PointTest.java
+++ MyShelfie/src/test/java/polimi/ingsw/Model/PointTest.java
@@ -1,16 +1,33 @@
 package polimi.ingsw.Model;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import polimi.ingsw.Model.Cards.Common.*;
+import polimi.ingsw.Model.Cards.Goal.CardGoal;
+import polimi.ingsw.Model.Chat.Message;
+import polimi.ingsw.Model.Enumeration.CardCommonType;
+import polimi.ingsw.Model.Enumeration.CardGoalType;
+import polimi.ingsw.Model.Enumeration.GameStatus;
+import polimi.ingsw.Model.Exceptions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PointTest {
+    Player p;
     @BeforeEach
     void setUp() {
+        //Set up the player point
+        p = new Player("Test");
+
+
     }
 
     @Test
     @DisplayName("Test player points")
     void testPlayerPoints() {
+        //Test the player points
+        Point point = new Point(0, CardCommonType.CommonX);
+        p.addPoint(point);
+        assertEquals(0, p.getTotalPoints());
     }
 }
