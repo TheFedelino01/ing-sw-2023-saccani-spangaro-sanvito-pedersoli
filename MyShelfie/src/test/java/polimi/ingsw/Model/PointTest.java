6714f40f76001259000d7efbe545984db2e7ecfa
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-04-23 17:02:52 +0200

    Update PointTest.java

--- MyShelfie/src/test/java/polimi/ingsw/Model/PointTest.java
+++ MyShelfie/src/test/java/polimi/ingsw/Model/PointTest.java
@@ -1,25 +1,21 @@
 package polimi.ingsw.Model;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import polimi.ingsw.Model.Cards.Common.*;
-import polimi.ingsw.Model.Cards.Goal.CardGoal;
-import polimi.ingsw.Model.Chat.Message;
 import polimi.ingsw.Model.Enumeration.CardCommonType;
-import polimi.ingsw.Model.Enumeration.CardGoalType;
-import polimi.ingsw.Model.Enumeration.GameStatus;
-import polimi.ingsw.Model.Exceptions.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PointTest {
-    Player p;
+    Player p1;
+    Player p2;
+
     @BeforeEach
     void setUp() {
         //Set up the player point
-        p = new Player("Test");
-
-
+        p1 = new Player("Test");
+        p2 = new Player("Test2");
     }
 
     @Test
@@ -27,7 +23,30 @@ public class PointTest {
     void testPlayerPoints() {
         //Test the player points
         Point point = new Point(0, CardCommonType.CommonX);
-        p.addPoint(point);
-        assertEquals(0, p.getTotalPoints());
+        p1.addPoint(point);
+        assertEquals(0, p1.getTotalPoints());
+        p2.addPoint(point);
+        assertEquals(0, p2.getTotalPoints());
+
+    }
+
+    @Test
+    @DisplayName("Check who wins?")
+    void testWhoWins() {
+        //Check who wins
+        Point point = new Point(0, CardCommonType.CommonX);
+        p1.addPoint(point);
+        p2.addPoint(point);
+        assertEquals(0, p1.getTotalPoints());
+        assertEquals(0, p2.getTotalPoints());
+        int x = p1.getTotalPoints();
+        int y = p2.getTotalPoints();
+        if (x > y) {
+            System.out.println("Player 1 wins!");
+        } else if (x < y) {
+            System.out.println("Player 2 wins!");
+        } else {
+            System.out.println("Draw!");
+        }
     }
 }
