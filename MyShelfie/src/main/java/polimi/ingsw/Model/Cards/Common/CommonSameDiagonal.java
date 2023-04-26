bf55ff76a181ca9289955da85abbd42bab24eccc
Francesco Spangaro <francescospangaro@gmail.com>
2023-04-26 19:33:14 +0200

    Cards Corrected same diagonal algorithm

--- MyShelfie/src/main/java/polimi/ingsw/Model/Cards/Common/CommonSameDiagonal.java
+++ MyShelfie/src/main/java/polimi/ingsw/Model/Cards/Common/CommonSameDiagonal.java
@@ -18,17 +18,17 @@ public class CommonSameDiagonal extends CommonCard {
             check2 = true;
             for (int r = i; r < DefaultValue.NumOfRowsShelf - 2 + i; r++) {
                 for (int c = 0; c < DefaultValue.NumOfColumnsShelf - 1; c++) {
-                    if (!toCheck.get(r, c).isSameType(toCheck.get(r + 1, c + 1).getType()) || toCheck.get(r, c).isSameType(TileType.NOT_USED))
+                    if ((!toCheck.get(r, c).isSameType(toCheck.get(r + 1, c + 1).getType()) || toCheck.get(r, c).isSameType(TileType.NOT_USED))&&r==c)
                         check1 = false;
                 }
             }
             for (int r = DefaultValue.NumOfRowsShelf - 1 - i; r > 1 - i; r--) {
                 for (int c = 0; c < DefaultValue.NumOfColumnsShelf - 1; c++) {
-                    if (!toCheck.get(r, c).isSameType(toCheck.get(r - 1, c + 1).getType()) || toCheck.get(r, c).isSameType(TileType.NOT_USED))
+                    if ((!toCheck.get(r, c).isSameType(toCheck.get(r - 1, c + 1).getType()) || toCheck.get(r, c).isSameType(TileType.NOT_USED))&&r==c)
                         check2 = false;
                 }
             }
         }
-        return check1&&check2;
+        return check1 || check2;
     }
 }
