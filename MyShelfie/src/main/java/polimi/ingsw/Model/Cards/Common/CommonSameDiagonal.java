d09b81279749728066831c4d615fb8faad6e9d5c
Francesco Spangaro <francescospangaro@gmail.com>
2023-04-27 11:15:23 +0200

    Cards Added diagonal cards test cases, corrected diagonal cards algorithm bug

--- MyShelfie/src/main/java/polimi/ingsw/Model/Cards/Common/CommonSameDiagonal.java
+++ MyShelfie/src/main/java/polimi/ingsw/Model/Cards/Common/CommonSameDiagonal.java
@@ -12,23 +12,34 @@ public class CommonSameDiagonal extends CommonCard {
 
     @Override
     public boolean verify(Shelf toCheck) {
-        boolean check1 = true, check2 = true;
+        int last, temp;
+        boolean check1, check2;
         for (int i = 0; i < 2; i++) {
+            last = DefaultValue.NumOfRowsShelf + 1;
             check1 = true;
             check2 = true;
-            for (int r = i; r < DefaultValue.NumOfRowsShelf - 2 + i; r++) {
-                for (int c = 0; c < DefaultValue.NumOfColumnsShelf - 1; c++) {
-                    if ((!toCheck.get(r, c).isSameType(toCheck.get(r + 1, c + 1).getType()) || toCheck.get(r, c).isSameType(TileType.NOT_USED))&&r==c)
-                        check1 = false;
+            for (int r = i; (r < DefaultValue.NumOfRowsShelf - 2 + i) && check1; r++) {
+                for (int c = 0; (c < DefaultValue.NumOfColumnsShelf - 1) && check1; c++) {
+                    if (i == 0) {
+                        if ((!toCheck.get(r, c).isSameType(toCheck.get(r + 1, c + 1).getType()) || toCheck.get(r, c).isSameType(TileType.NOT_USED)) && r == c)
+                            check1 = false;
+                    } else {
+                        if ((!toCheck.get(r, c).isSameType(toCheck.get(r + 1, c + 1).getType()) || toCheck.get(r, c).isSameType(TileType.NOT_USED)) && r == c + 1)
+                            check1 = false;
+                    }
                 }
             }
-            for (int r = DefaultValue.NumOfRowsShelf - 1 - i; r > 1 - i; r--) {
-                for (int c = 0; c < DefaultValue.NumOfColumnsShelf - 1; c++) {
-                    if ((!toCheck.get(r, c).isSameType(toCheck.get(r - 1, c + 1).getType()) || toCheck.get(r, c).isSameType(TileType.NOT_USED))&&r==c)
+            for (int r = DefaultValue.NumOfRowsShelf - 1 - i; (r > 1 - i) && check2; r--) {
+                last--;
+                for (int c = DefaultValue.NumOfColumnsShelf - 2; (c > -1) && check2; c--) {
+                    temp = c;
+                    if ((!toCheck.get(r, c).isSameType(toCheck.get(r - 1, c + 1).getType()) || toCheck.get(r, c).isSameType(TileType.NOT_USED)) && (temp == last))
                         check2 = false;
                 }
             }
+            if (check1 || check2)
+                return true;
         }
-        return check1 || check2;
+        return false;
     }
 }
--- MyShelfie/src/test/java/polimi/ingsw/Model/Cards/Common/CommonDiagonalTest.java
+++ MyShelfie/src/test/java/polimi/ingsw/Model/Cards/Common/CommonDiagonalTest.java
@@ -28,7 +28,13 @@ public class CommonDiagonalTest {
 
     //I'll be using the CAT tile as the one to assert its value
     /*
-
+    C = CAT, N = NOT_USED, R = RANDOM_TILE
+        C N N N N
+        R C N N N
+        R R C N N
+        R R R C N
+        R R R R C
+        R R R R R
      */
     @Test
     @DisplayName("Test Diagonal cards from top left to bottom right")
@@ -54,6 +60,47 @@ public class CommonDiagonalTest {
     }
 
 
+    /*
+    C = CAT, N = NOT_USED, R = RANDOM_TILE
+        N N N N N
+        C N N N N
+        R C N N N
+        R R C N N
+        R R R C N
+        R R R R C
+     */
+    @Test
+    @DisplayName("Test Diagonal first row empty")
+    public void testDiagonalTB2() {
+        Shelf test = new Shelf();
+        for (int i = 1; i < DefaultValue.NumOfRowsShelf; i++) {
+            for (int j = 0; j < DefaultValue.NumOfColumnsShelf; j++) {
+                if (i - 1 >= j) {
+                    if (i - 1 == j) {
+                        test.setSingleTile(new Tile(TileType.CAT), i, j);
+                    } else {
+                        test.setSingleTile(new Tile(TileType.randomTileCAT()), i, j);
+                    }
+                }
+            }
+        }
+        assertTrue(model.stream()
+                .filter(x -> x.getCommonType().equals(CardCommonType.CommonSameDiagonal))
+                .toList().get(0).verify(test));
+        assertTrue(model.stream()
+                .filter(x -> x.getCommonType().equals(CardCommonType.CommonStair))
+                .toList().get(0).verify(test));
+    }
+
+    /*
+        C = CAT, N = NOT_USED, R = RANDOM_TILE
+            N N N N C
+            N N N C R
+            N N C R R
+            N C R R R
+            C R R R R
+            R R R R R
+         */
     @Test
     @DisplayName("Test Diagonal cards from bottom left to top right")
     public void testDiagonalBT() {
@@ -77,6 +124,45 @@ public class CommonDiagonalTest {
                 .toList().get(0).verify(test));
     }
 
+    /*
+        C = CAT, N = NOT_USED, R = RANDOM_TILE
+            N N N N N
+            N N N N C
+            N N N C R
+            N N C R R
+            N C R R R
+            C R R R R
+         */
+
+    @Test
+    @DisplayName("Test Diagonal first row empty")
+    public void testDiagonalBT2() {
+        int last = DefaultValue.NumOfColumnsShelf + 1, temp;
+        boolean check;
+        Shelf test = new Shelf();
+        for (int r = 0; r < DefaultValue.NumOfRowsShelf; r++) {
+            last--;
+            check = true;
+            for (int c = DefaultValue.NumOfColumnsShelf - 1; c > -1; c--) {
+                temp = c;
+                if (r > 0) {
+                    if (last == temp) {
+                        test.setSingleTile(new Tile(TileType.CAT), r, c);
+                        check = false;
+                    } else if (check) {
+                        test.setSingleTile(new Tile(TileType.randomTileCAT()), r, c);
+                    }
+                }
+            }
+        }
+        assertTrue(model.stream()
+                .filter(x -> x.getCommonType().equals(CardCommonType.CommonSameDiagonal))
+                .toList().get(0).verify(test));
+        assertTrue(model.stream()
+                .filter(x -> x.getCommonType().equals(CardCommonType.CommonStair))
+                .toList().get(0).verify(test));
+    }
+
     //I copied methods from other test classes to initialise the shelf,
     //so that it cannot verify the diagonal condition
     @Test
@@ -85,7 +171,7 @@ public class CommonDiagonalTest {
         Shelf test = new Shelf();
         for (int i = 0; i < DefaultValue.NumOfRowsShelf; i++) {
             for (int j = 0; j < DefaultValue.NumOfColumnsShelf; j++) {
-                switch(j){
+                switch (j) {
                     case 0 -> test.setSingleTile(new Tile(TileType.CAT), i, j);
                     case 1 -> test.setSingleTile(new Tile(TileType.BOOK), i, j);
                     case 2 -> test.setSingleTile(new Tile(TileType.TROPHY), i, j);
