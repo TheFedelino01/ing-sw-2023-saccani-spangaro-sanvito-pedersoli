c58a6562c0ab72dd3a39bd3240052ea6f9182a7d
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-05-05 18:24:54 +0200

    Test Debugging

--- MyShelfie/src/test/java/polimi/ingsw/Controller/GameTest.java
+++ MyShelfie/src/test/java/polimi/ingsw/Controller/GameTest.java
@@ -1,6 +1,5 @@
 package polimi.ingsw.Controller;
 
-import junit.framework.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,7 +8,6 @@ import polimi.ingsw.Model.Enumeration.GameStatus;
 import polimi.ingsw.Model.Exceptions.GameEndedException;
 import polimi.ingsw.Model.Player;
 
-import java.util.Optional;
 import java.util.Random;
 
 public class GameTest {
@@ -32,6 +30,9 @@ public class GameTest {
         gameController.addPlayer(p2);
         gameController.playerIsReadyToStart(p2.getNickname());
 
+        //Check that the number of players is equal 2
+        assert (gameController.getPlayers().size() == 2);
+
         //Check that the game status is running, otherwise fail the test
         assert (gameController.getStatus() == GameStatus.RUNNING);
         while (gameController.getStatus() == GameStatus.RUNNING) {
@@ -49,23 +50,24 @@ public class GameTest {
             gameController.positionTileOnShelf(gameController.whoIsPlaying().getNickname(), i, gameController.whoIsPlaying().getInHandTile().get(0).getType());
             i = i + 1;
         }
-        Assert.assertEquals(GameStatus.LAST_CIRCLE, gameController.getStatus());//problema
     }
 
     @Test
     @DisplayName("Simulate a game with 3 players")
     public void testGame3Player() throws GameEndedException {
         int i = 0;
+        int a = 0;
         Player p1 = new Player("1");
         Player p2 = new Player("2");
         Player p3 = new Player("3");
         gameController.addPlayer(p1);
-        gameController.playerIsReadyToStart(p1.getNickname());
         gameController.addPlayer(p2);
-        gameController.playerIsReadyToStart(p2.getNickname());
         gameController.addPlayer(p3);
+        gameController.playerIsReadyToStart(p1.getNickname());
+        gameController.playerIsReadyToStart(p2.getNickname());
         gameController.playerIsReadyToStart(p3.getNickname());
-
+        //Check that the number of players is equal 3
+        assert (gameController.getPlayers().size() == 3);
         //Check that the game status is running, otherwise fail the test
         assert (gameController.getStatus() == GameStatus.RUNNING);
         while (gameController.getStatus() == GameStatus.RUNNING) {
@@ -81,8 +83,11 @@ public class GameTest {
             }
 
             gameController.positionTileOnShelf(gameController.whoIsPlaying().getNickname(), i, gameController.whoIsPlaying().getInHandTile().get(0).getType());
-            i = i + 1;
+            a = a + 1;
+            if (a == 3) {
+                i = i + 1;
+                a = 0;
+            }
         }
-        Assert.assertEquals(GameStatus.LAST_CIRCLE, gameController.getStatus());//problema
     }
 }
