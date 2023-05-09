f0e7b43dd9f1b1ef0eee9e42c64335ca6472a85d
Federico Saccani <federico.saccani01@gmail.com>
2023-05-09 16:34:59 +0200

    Moved all in InGameController for best flexibility

--- MyShelfie/src/main/java/polimi/ingsw/View/userView/gui/GUIApplication.java
+++ MyShelfie/src/main/java/polimi/ingsw/View/userView/gui/GUIApplication.java
@@ -7,6 +7,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import polimi.ingsw.Model.DefaultValue;
@@ -19,6 +20,7 @@ import polimi.ingsw.View.userView.GameFlow;
 import polimi.ingsw.View.userView.gui.controllers.*;
 import polimi.ingsw.View.userView.utilities.inputReaderGUI;
 
+import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -26,13 +28,13 @@ public class GUIApplication extends Application {
 
     private GameFlow gameFlow;
 
-    private Stage primaryStage,popUpStage;
+    private Stage primaryStage, popUpStage;
     private StackPane root;
     private ArrayList<SceneInfo> scenes;
 
     @Override
     public void start(Stage primaryStage) {
-        gameFlow=new GameFlow(this,ConnectionSelection.valueOf(getParameters().getUnnamed().get(0)));
+        gameFlow = new GameFlow(this, ConnectionSelection.valueOf(getParameters().getUnnamed().get(0)));
         loadScenes();
 
         this.primaryStage = primaryStage;
@@ -41,43 +43,44 @@ public class GUIApplication extends Application {
         root = new StackPane();
     }
 
-    private void loadScenes(){
+    private void loadScenes() {
         //Loads all the scenes available to be showed during the game
         scenes = new ArrayList<>();
         FXMLLoader loader;
         Parent root;
         GenericController gc;
-        for(int i=0; i<SceneEnum.values().length;i++){
+        for (int i = 0; i < SceneEnum.values().length; i++) {
             loader = new FXMLLoader(getClass().getResource(SceneEnum.values()[i].value()));
             try {
                 root = loader.load();
-                gc=loader.getController();
+                gc = loader.getController();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-            scenes.add(new SceneInfo(new Scene(root),SceneEnum.values()[i],gc));
+            scenes.add(new SceneInfo(new Scene(root), SceneEnum.values()[i], gc));
         }
     }
 
     public void setInputReaderGUItoAllControllers(inputReaderGUI inputReaderGUI) {
-        for(SceneInfo s:scenes){
+        for (SceneInfo s : scenes) {
             s.setInputReaderGUI(inputReaderGUI);
         }
     }
 
-    public GenericController getController(SceneEnum scene){
+    public GenericController getController(SceneEnum scene) {
         int index = getSceneIndex(scene);
-        if(index!=-1) {
+        if (index != -1) {
             return scenes.get(getSceneIndex(scene)).getGenericController();
         }
         return null;
     }
-    public void setActiveScene(SceneEnum scene){
-        int index = getSceneIndex(scene);
-        if(index!=-1) {
 
-            switch (scene){
-                case NICKNAME_POPUP ->{
+    public void setActiveScene(SceneEnum scene) {
+        int index = getSceneIndex(scene);
+        if (index != -1) {
+
+            switch (scene) {
+                case NICKNAME_POPUP -> {
                     openPopup(scenes.get(getSceneIndex(scene)).getScene());
                     return;
                 }
@@ -86,7 +89,7 @@ public class GUIApplication extends Application {
                     this.primaryStage.setAlwaysOnTop(true);
                     this.primaryStage.centerOnScreen();
                 }
-                case MENU->{
+                case MENU -> {
                     this.primaryStage.centerOnScreen();
                     this.primaryStage.setAlwaysOnTop(false);
                 }
@@ -100,33 +103,33 @@ public class GUIApplication extends Application {
         }
     }
 
-    public void showPlayerToLobby(GameModelImmutable model){
+    public void showPlayerToLobby(GameModelImmutable model) {
         hidePanesInLobby();
-        int i=0;
-        for(Player p:model.getPlayers()){
-            addLobbyPanePlayer(p.getNickname(),i, p.getReadyToStart());
+        int i = 0;
+        for (Player p : model.getPlayers()) {
+            addLobbyPanePlayer(p.getNickname(), i, p.getReadyToStart());
             i++;
         }
     }
 
-    private void addLobbyPanePlayer(String nick, int indexPlayer, boolean isReady){
-        SceneEnum se=null;
-        switch(indexPlayer){
-            case 0->se=SceneEnum.PLAYER_LOBBY_CARD0;
-            case 1->se=SceneEnum.PLAYER_LOBBY_CARD1;
-            case 2->se=SceneEnum.PLAYER_LOBBY_CARD2;
-            case 3->se=SceneEnum.PLAYER_LOBBY_CARD3;
+    private void addLobbyPanePlayer(String nick, int indexPlayer, boolean isReady) {
+        SceneEnum se = null;
+        switch (indexPlayer) {
+            case 0 -> se = SceneEnum.PLAYER_LOBBY_CARD0;
+            case 1 -> se = SceneEnum.PLAYER_LOBBY_CARD1;
+            case 2 -> se = SceneEnum.PLAYER_LOBBY_CARD2;
+            case 3 -> se = SceneEnum.PLAYER_LOBBY_CARD3;
         }
         Pane paneToLoad;
         SceneInfo sceneToLoad = scenes.get(getSceneIndex(se));
-        paneToLoad = (Pane)sceneToLoad.getScene().getRoot();
+        paneToLoad = (Pane) sceneToLoad.getScene().getRoot();
 
-        ((PlayerLobbyCardController)sceneToLoad.getGenericController()).setNickname(nick);
+        ((PlayerLobbyCardController) sceneToLoad.getGenericController()).setNickname(nick);
 
         Pane paneReady = (Pane) this.primaryStage.getScene().getRoot().lookup("#ready" + indexPlayer);
         paneReady.setVisible(isReady);
 
-        Pane panePlayerLobby = (Pane)this.primaryStage.getScene().getRoot().lookup("#pane"+indexPlayer);
+        Pane panePlayerLobby = (Pane) this.primaryStage.getScene().getRoot().lookup("#pane" + indexPlayer);
         panePlayerLobby.setVisible(true);
 
         panePlayerLobby.getChildren().clear();
@@ -145,8 +148,8 @@ public class GUIApplication extends Application {
 
     }
 
-    private void hidePanesInLobby(){
-        for(int i=0; i<4;i++) {
+    private void hidePanesInLobby() {
+        for (int i = 0; i < 4; i++) {
             Pane panePlayerLobby = (Pane) this.primaryStage.getScene().getRoot().lookup("#pane" + i);
             panePlayerLobby.setVisible(false);
 
@@ -154,7 +157,8 @@ public class GUIApplication extends Application {
             paneReady.setVisible(false);
         }
     }
-    private void openPopup(Scene scene){
+
+    private void openPopup(Scene scene) {
         popUpStage = new Stage();
         popUpStage.setTitle("Info");
         popUpStage.setResizable(false);
@@ -167,78 +171,33 @@ public class GUIApplication extends Application {
         popUpStage.setX(primaryStage.getX() + (primaryStage.getWidth() - scene.getWidth()) * 0.5);
         popUpStage.setY(primaryStage.getY() + (primaryStage.getHeight() - scene.getHeight()) * 0.5);
     }
-    public void closePopUpStage(){
-        if(popUpStage!=null)
+
+    public void closePopUpStage() {
+        if (popUpStage != null)
             popUpStage.hide();
     }
 
     public void disableBtnReadyToStart() {
         //I set not visible the btn "Ready to start"
-        ((LobbyController)scenes.get(getSceneIndex(SceneEnum.LOBBY)).getGenericController()).setVisibleBtnReady(false);
+        ((LobbyController) scenes.get(getSceneIndex(SceneEnum.LOBBY)).getGenericController()).setVisibleBtnReady(false);
     }
 
     private int getSceneIndex(SceneEnum sceneName) {
-        for(int i = 0; i < scenes.size(); i++){
-            if(scenes.get(i).getSceneEnum().equals(sceneName))
+        for (int i = 0; i < scenes.size(); i++) {
+            if (scenes.get(i).getSceneEnum().equals(sceneName))
                 return i;
         }
         return -1;
     }
 
-    public void showInGameModel(GameModelImmutable model){
-        showTilesToPlayground(model);
-        showCommonCards(model);
-        showPersonalCard(model);
-    }
-    private void showTilesToPlayground(GameModelImmutable model){
-        Tile t;
-        Pane tilePane;
-        //AnchorPane anchor = (AnchorPane) this.primaryStage.getScene().getRoot().lookup("#anchorMain");
-        for(int r=0; r< DefaultValue.PlaygroundSize;r++){
-            for(int c=0; c< DefaultValue.PlaygroundSize;c++) {
-                t = model.getPg().getTile(r,c);
+    public void showInGameModel(GameModelImmutable model) {
+        InGameController controller = (InGameController) scenes.get(getSceneIndex(SceneEnum.INGAME)).getGenericController();
+        controller.setNickname(model);
+        controller.setPlayground(model);
+        controller.setCommonCards(model);
 
-                tilePane= (Pane) this.primaryStage.getScene().getRoot().lookup("#pg" + r+c);
-
-                if(tilePane!=null){
-                    if(!t.getType().equals(TileType.NOT_USED) &&  !t.getType().equals(TileType.USED) && !t.getType().equals(TileType.FINISHED_USING) ) {
-                        tilePane.getStyleClass().add(t.getType().getBackgroundClass());
-                        tilePane.getStyleClass().add("tileHover");
-                        tilePane.setVisible(true);
-                    }else{
-                        tilePane.setVisible(false);
-                    }
-                }
-
-            }
-        }
-    }
-
-    private void showCommonCards(GameModelImmutable model){
-        Pane tilePane;
-        tilePane= (Pane) this.primaryStage.getScene().getRoot().lookup("#cc0");
-
-        tilePane.getStyleClass().add(model.getCommonCards().get(0).getCommonType().getBackgroundClass());
-        tilePane.getStyleClass().add("tileHover");
-        tilePane.setVisible(true);
-
-        tilePane= (Pane) this.primaryStage.getScene().getRoot().lookup("#cc1");
-        tilePane.getStyleClass().add(model.getCommonCards().get(1).getCommonType().getBackgroundClass());
-        tilePane.getStyleClass().add("tileHover");
-        tilePane.setVisible(true);
-
-    }
-
-    private void showPersonalCard(GameModelImmutable model){
-        Pane tilePane;
-        for(int i=0;i<model.getPlayers().size();i++){
-            tilePane= (Pane) this.primaryStage.getScene().getRoot().lookup("#pc"+(i+1));
-            tilePane.getStyleClass().add(model.getPlayers().get(i).getSecretGoal().getGoalType().getBackgroundClass());
-            tilePane.getStyleClass().add("tileHover");
-            tilePane.setVisible(true);
-        }
-
-
+        controller.setInvisibleAllShelves();
+        controller.setVisibleShelves(model);
     }
 
 
@@ -270,11 +229,9 @@ public class GUIApplication extends Application {
     }
 
 
-
     public static void main(String[] args) {
         launch(args);
     }
-
 
 
 }
--- MyShelfie/src/main/java/polimi/ingsw/View/userView/gui/controllers/InGameController.java
+++ MyShelfie/src/main/java/polimi/ingsw/View/userView/gui/controllers/InGameController.java
@@ -1,4 +1,118 @@
 package polimi.ingsw.View.userView.gui.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import polimi.ingsw.Model.DefaultValue;
+import polimi.ingsw.Model.Enumeration.TileType;
+import polimi.ingsw.Model.GameModelView.GameModelImmutable;
+import polimi.ingsw.Model.Tile;
+
 public class InGameController extends GenericController{
+
+    @FXML
+    private Label lablePlayer1;
+    @FXML
+    private Label lablePlayer2;
+    @FXML
+    private Label lablePlayer3;
+    @FXML
+    private Label lablePlayer4;
+
+    @FXML
+    private Pane tilesPane;
+
+    @FXML
+    private AnchorPane mainAnchor;
+
+    public void setNickname(GameModelImmutable model){
+        int playerNum = model.getPlayers().size();
+        for (int i = 0; i < playerNum; i++) {
+            switch (i){
+                case 0->{
+                    lablePlayer1.setText(model.getPlayers().get(i).getNickname());
+                }
+                case 1->{
+                    lablePlayer2.setText(model.getPlayers().get(i).getNickname());
+                }
+                case 2->{
+                    lablePlayer3.setText(model.getPlayers().get(i).getNickname());
+                }
+                case 3->{
+                    lablePlayer4.setText(model.getPlayers().get(i).getNickname());
+                }
+            }
+        }
+    }
+
+    public void setPlayground(GameModelImmutable model){
+        Tile t;
+        Pane tilePane;
+        for (int r = 0; r < DefaultValue.PlaygroundSize; r++) {
+            for (int c = 0; c < DefaultValue.PlaygroundSize; c++) {
+                t = model.getPg().getTile(r, c);
+
+                tilePane = (Pane) tilesPane.lookup("#pg" + r + c);
+
+                if (tilePane != null) {
+                    if (!t.getType().equals(TileType.NOT_USED) && !t.getType().equals(TileType.USED) && !t.getType().equals(TileType.FINISHED_USING)) {
+                        tilePane.getStyleClass().add(t.getType().getBackgroundClass());
+                        tilePane.getStyleClass().add("tileHover");
+                        tilePane.setVisible(true);
+                    } else {
+                        tilePane.setVisible(false);
+                    }
+                }
+
+            }
+        }
+    }
+
+    public void setCommonCards(GameModelImmutable model){
+        Pane tilePane;
+        tilePane = (Pane) mainAnchor.lookup("#cc0");
+
+        tilePane.getStyleClass().add(model.getCommonCards().get(0).getCommonType().getBackgroundClass());
+        tilePane.getStyleClass().add("tileHover");
+        tilePane.setVisible(true);
+
+        tilePane = (Pane) mainAnchor.lookup("#cc1");
+        tilePane.getStyleClass().add(model.getCommonCards().get(1).getCommonType().getBackgroundClass());
+        tilePane.getStyleClass().add("tileHover");
+        tilePane.setVisible(true);
+    }
+
+    public void setInvisibleAllShelves(){
+        Pane pane;
+        for(int i=1;i<=DefaultValue.MaxNumOfPlayer;i++){
+            pane = (Pane) mainAnchor.lookup("#player" + (i));
+            pane.setVisible(false);
+        }
+    }
+
+    public void setVisibleShelves(GameModelImmutable model){
+        Pane pane;
+
+        int playerNum = model.getPlayers().size();
+        //Show personal goal and shelf of the player in game
+        for (int i = 0; i < playerNum; i++) {
+            pane = (Pane) mainAnchor.lookup("#player" + (i + 1));
+            pane.setVisible(true);
+
+            showPersonalCard(i+1,model.getPlayers().get(i).getSecretGoal().getGoalType().getBackgroundClass());
+        }
+    }
+
+    private void showPersonalCard(int playerIndex, String backgroundClass) {
+        Pane tilePane;
+        tilePane = (Pane) mainAnchor.lookup("#pc" + playerIndex);
+        tilePane.getStyleClass().add(backgroundClass);
+        tilePane.getStyleClass().add("tileHover");
+        tilePane.setVisible(true);
+
+    }
+
 }
