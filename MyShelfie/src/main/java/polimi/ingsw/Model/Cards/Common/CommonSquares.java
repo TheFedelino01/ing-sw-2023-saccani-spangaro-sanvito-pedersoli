0de3b335fb44eb9dcd812d18c63d5363c200742b
Francesco Spangaro <francescospangaro@gmail.com>
2023-05-15 00:31:16 +0200

    Corrected squares algorithm closes #24

--- MyShelfie/src/main/java/polimi/ingsw/Model/Cards/Common/CommonSquares.java
+++ MyShelfie/src/main/java/polimi/ingsw/Model/Cards/Common/CommonSquares.java
@@ -4,6 +4,10 @@ import polimi.ingsw.Model.DefaultValue;
 import polimi.ingsw.Model.Enumeration.CardCommonType;
 import polimi.ingsw.Model.Enumeration.TileType;
 import polimi.ingsw.Model.Shelf;
+import polimi.ingsw.Model.Tile;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class CommonSquares extends CommonMethods {
     public CommonSquares(CardCommonType type) {
@@ -12,21 +16,35 @@ public class CommonSquares extends CommonMethods {
 
     @Override
     public boolean verify(Shelf toCheck) {
+        Integer row = null, col = null;
+        Shelf temp = new Shelf();
+        for (int r = 0; r < DefaultValue.NumOfRowsShelf; r++) {
+            for (int c = 0; c < DefaultValue.NumOfColumnsShelf; c++) {
+                temp.setSingleTile(new Tile(toCheck.get(r, c).getType()), r, c);
+            }
+        }
         int sum = 0;
-        for (int i = 0; i < DefaultValue.NumOfRowsShelf; i++) //vertical check
-        {
-            for (int j = 0; j < DefaultValue.NumOfColumnsShelf; j++) {
-                if (i < DefaultValue.NumOfRowsShelf - 1 && j < DefaultValue.NumOfColumnsShelf - 1) {   //analyse square
-                    if (!toCheck.get(i, j).isSameType(TileType.NOT_USED) &&
-                            toCheck.get(i, j).isSameType(toCheck.get(i + 1, j).getType()) &&
-                            toCheck.get(i, j).isSameType(toCheck.get(i, j + 1).getType()) &&
-                            toCheck.get(i, j).isSameType(toCheck.get(i + 1, j + 1).getType())) {
-                        sum++;
-                        if (sum == 2) {
-                            return true;
+        for (TileType t : TileType.getUsableValues()) {
+            for (int r = 0; r < DefaultValue.NumOfRowsShelf - 1; r++) {
+                for (int c = 0; c < DefaultValue.NumOfColumnsShelf - 1; c++) {
+                    if (temp.get(r, c).isSameType(temp.get(r + 1, c).getType()) &&
+                        temp.get(r, c).isSameType(temp.get(r, c + 1).getType()) &&
+                        temp.get(r, c).isSameType(temp.get(r + 1, c + 1).getType()) &&
+                        !temp.get(r, c).isSameType(TileType.NOT_USED) && !temp.get(r, c).isSameType(TileType.FINISHED_USING)) {
+
+                        if (sum == 1) {
+                            if(r != row && c != col){
+                                return true;
+                            }
+                        } else {
+                            row = r + 2;
+                            col = c + 2;
+                            temp.setSingleTile(new Tile(TileType.NOT_USED), r, c);
+                            temp.setSingleTile(new Tile(TileType.NOT_USED), r + 1, c);
+                            temp.setSingleTile(new Tile(TileType.NOT_USED), r, c + 1);
+                            temp.setSingleTile(new Tile(TileType.NOT_USED), r + 1, c + 1);
+                            sum++;
                         }
-                        //delete all the same adjacent elements
-                        deleteAdjacent(toCheck, i, j, toCheck.get(i, j));
                     }
                 }
             }
