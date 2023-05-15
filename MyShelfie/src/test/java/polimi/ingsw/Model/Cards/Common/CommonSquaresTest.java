4b3c616e08665229392734649bba88e5b4e78371
TheFedelino01 <federico.saccani01@gmail.com>
2023-05-15 20:21:36 +0200

    Added more CommonSquaresTest

--- MyShelfie/src/test/java/polimi/ingsw/Model/Cards/Common/CommonSquaresTest.java
+++ MyShelfie/src/test/java/polimi/ingsw/Model/Cards/Common/CommonSquaresTest.java
@@ -185,4 +185,175 @@ public class CommonSquaresTest extends ShelfConverter{
 
         assertTrue(card.verify(test));
     }
+
+    @Test
+    @DisplayName("2 Groups of 2x2 box tiles with same Tiles inside group #11")
+    public void testFourGroups11() {
+        String[][] matrix = {
+                {"","","","",""},
+                {"","","","",""},
+                {"A","","T","C","C"},
+                {"A","","T","C","C"},
+                {"A","A","T","T","T"},
+                {"A","A","T","T","B"}
+        };
+
+        Shelf test = setShelf(matrix);
+
+        assertTrue(card.verify(test));
+    }
+
+    @Test
+    @DisplayName("2 Groups of 2x2 box tiles with same Tiles inside group #12")
+    public void testFourGroups12() {
+        String[][] matrix = {
+                {"","","","",""},
+                {"","","","",""},
+                {"A","","C","C","C"},
+                {"A","","C","C","C"},
+                {"A","A","C","T","T"},
+                {"A","A","T","T","B"}
+        };
+
+        Shelf test = setShelf(matrix);
+
+        assertFalse(card.verify(test));
+    }
+
+    @Test
+    @DisplayName("2 Groups of 2x2 box tiles with same Tiles inside group #13")
+    public void testFourGroups13() {
+        String[][] matrix = {
+                {"T","T","","",""},
+                {"T","T","","",""},
+                {"A","","C","C","C"},
+                {"A","","C","C","C"},
+                {"A","C","C","C","C"},
+                {"A","C","C","C","C"}
+        };
+
+        Shelf test = setShelf(matrix);
+
+        assertTrue(card.verify(test));
+    }
+
+
+    @Test
+    @DisplayName("2 Groups of 2x2 box tiles with same Tiles inside group #14")
+    public void testFourGroups14() {
+        String[][] matrix = {
+                {"T","T","","",""},
+                {"T","T","","",""},
+                {"A","","C","C","C"},
+                {"A","","C","C","C"},
+                {"A","A","C","C","C"},
+                {"A","A","C","C","C"}
+        };
+
+        Shelf test = setShelf(matrix);
+
+        assertTrue(card.verify(test));
+    }
+
+    @Test
+    @DisplayName("2 Groups of 2x2 box tiles with same Tiles inside group #15")
+    public void testFourGroups15() {
+        String[][] matrix = {
+                {"T","T","","",""},
+                {"T","T","","",""},
+                {"A","A","C","C","C"},
+                {"A","A","C","C","C"},
+                {"A","A","C","C","C"},
+                {"A","A","C","C","C"}
+        };
+
+        Shelf test = setShelf(matrix);
+
+        assertTrue(card.verify(test));
+    }
+
+    @Test
+    @DisplayName("2 Groups of 2x2 box tiles with same Tiles inside group #16")
+    public void testFourGroups16() {
+        String[][] matrix = {
+                {"T","T","","",""},
+                {"T","T","","",""},
+                {"A","C","C","C","C"},
+                {"A","A","C","C","C"},
+                {"A","A","C","C","C"},
+                {"A","A","C","C","C"}
+        };
+
+        Shelf test = setShelf(matrix);
+
+        assertFalse(card.verify(test));
+    }
+
+    @Test
+    @DisplayName("2 Groups of 2x2 box tiles with same Tiles inside group #17")
+    public void testFourGroups17() {
+        String[][] matrix = {
+                {"","","","",""},
+                {"","","","",""},
+                {"T","T","","",""},
+                {"T","T","","",""},
+                {"A","T","A","C","C"},
+                {"A","T","A","C","C"}
+        };
+
+        Shelf test = setShelf(matrix);
+
+        assertTrue(card.verify(test));
+    }
+
+    @Test
+    @DisplayName("2 Groups of 2x2 box tiles with same Tiles inside group #18")
+    public void testFourGroups18() {
+        String[][] matrix = {
+                {"","","","",""},
+                {"","","","",""},
+                {"T","T","","",""},
+                {"T","T","","",""},
+                {"C","C","C","C","C"},
+                {"C","C","C","C","C"}
+        };
+
+        Shelf test = setShelf(matrix);
+
+        assertFalse(card.verify(test));
+    }
+
+    @Test
+    @DisplayName("2 Groups of 2x2 box tiles with same Tiles inside group #19")
+    public void testFourGroups19() {
+        String[][] matrix = {
+                {"C","","","T","T"},
+                {"","C","","T","T"},
+                {"","","C","","T"},
+                {"A","A","A","C","T"},
+                {"A","A","A","C","T"},
+                {"C","C","C","T","T"}
+        };
+
+        Shelf test = setShelf(matrix);
+
+        assertTrue(card.verify(test));
+    }
+
+    @Test
+    @DisplayName("2 Groups of 2x2 box tiles with same Tiles inside group #20")
+    public void testFourGroups20() {
+        String[][] matrix = {
+                {"","","","T","T"},
+                {"","","","T","T"},
+                {"","","","C","T"},
+                {"","","","C","T"},
+                {"A","A","","A","T"},
+                {"A","A","A","A","T"}
+        };
+
+        Shelf test = setShelf(matrix);
+
+        assertFalse(card.verify(test));
+    }
 }
