9384caeb507347daecc789dc3129bd78ae0e43f9
TheFedelino01 <federico.saccani01@gmail.com>
2023-05-15 19:41:41 +0200

    Made Abstract class ShelfConverter

--- MyShelfie/src/test/java/polimi/ingsw/Model/Cards/Common/CommonFourGroupsTest.java
+++ MyShelfie/src/test/java/polimi/ingsw/Model/Cards/Common/CommonFourGroupsTest.java
@@ -13,7 +13,7 @@ import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
 
 
-public class CommonFourGroupsTest {
+public class CommonFourGroupsTest extends ShelfConverter{
 
     private CommonFourGroups card;
     @BeforeEach
@@ -198,28 +198,5 @@ public class CommonFourGroupsTest {
         assertTrue(card.verify(test));
     }
 
-    private Shelf setShelf(String[][] shelf){
-        int rows= DefaultValue.NumOfRowsShelf;
-        int cols = DefaultValue.NumOfColumnsShelf;
-        Shelf ris = new Shelf();
-        TileType tmp=null;
-        for(int r=0; r< rows;r++){
-            for(int c=0; c<cols;c++){
-                switch (shelf[r][c].toUpperCase()){
-                    case "C"->tmp=TileType.CAT;
-                    case "B"->tmp=TileType.BOOK;
-                    case "A"->tmp=TileType.ACTIVITY;
-                    case "F"->tmp=TileType.FRAME;
-                    case "T"->tmp=TileType.TROPHY;
-                    case "P"->tmp=TileType.PLANT;
-                    case "FIN"->tmp=TileType.FINISHED_USING;
-                    case "X"->tmp=TileType.NOT_USED;
-                    case ""->tmp=TileType.NOT_USED;
-                }
 
-                ris.setSingleTile(new Tile(tmp),r,c);
-            }
-        }
-        return ris;
-    }
 }
