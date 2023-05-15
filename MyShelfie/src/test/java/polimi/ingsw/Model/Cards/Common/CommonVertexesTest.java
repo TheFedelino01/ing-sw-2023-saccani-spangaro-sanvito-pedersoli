856a47c9796d407865a94356a42e75e393818057
TheFedelino01 <federico.saccani01@gmail.com>
2023-05-15 19:50:18 +0200

    Added Test of CommonVertexesTest

--- MyShelfie/src/test/java/polimi/ingsw/Model/Cards/Common/CommonVertexesTest.java
+++ MyShelfie/src/test/java/polimi/ingsw/Model/Cards/Common/CommonVertexesTest.java
@@ -14,11 +14,11 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static polimi.ingsw.Model.Enumeration.CardCommonType.CommonVertex;
 
-public class CommonVertexesTest {
+public class CommonVertexesTest extends ShelfConverter{
 
-    List<CommonCard> model = new ArrayList<>();
-
+    private CommonVertixesCard card;
     /**
      * Legend:<br>
      * N means that the tile has not been set<br>
@@ -27,64 +27,167 @@ public class CommonVertexesTest {
      */
     @BeforeEach
     void setUp() {
-        CommonCardFactory c = new CommonCardFactory();
-        for (CardCommonType t : CardCommonType.values())
-            model.add(c.getCommonCard(t));
+        card = (CommonVertixesCard)CommonCardFactory.getCommonCard(CommonVertex);
     }
 
-    /**
-    C X X X C<br>
-    X X X X X<br>
-    X X X X X<br>
-    X X X X X<br>
-    X X X X X<br>
-    C X X X C<br>
-     */
-    @Test
-    @DisplayName("Test vertexes")
-    public void testVertexes() {
-        Shelf test = new Shelf();
-        for (int i = 0; i < DefaultValue.NumOfRowsShelf; i++) {
-            for (int j = 0; j < DefaultValue.NumOfColumnsShelf; j++) {
-                if ((i == 0 && j == 0) ||
-                        (i == DefaultValue.NumOfRowsShelf - 1 && j == DefaultValue.NumOfColumnsShelf - 1) ||
-                        (i == 0 && j == DefaultValue.NumOfColumnsShelf - 1) ||
-                        (i == DefaultValue.NumOfRowsShelf - 1 && j == 0))
-                    test.setSingleTile(new Tile(TileType.CAT), i, j);
 
-                else
-                    test.setSingleTile(new Tile(TileType.randomTileCAT()), i, j);
-            }
-        }
-        assertTrue(model.get(1).verify(test));
+    @Test
+    @DisplayName("Test vertexes #0")
+    public void testVertexes0() {
+        String[][] matrix = {
+                {"","","","",""},
+                {"","","","",""},
+                {"","","","",""},
+                {"","","","",""},
+                {"","","","",""},
+                {"","","","",""}
+        };
+        Shelf test = this.setShelf(matrix);
+        assertFalse(card.verify(test));
     }
 
     @Test
-    @DisplayName("Test expected to return false")
-    public void testFail() {
-        Shelf test = new Shelf();
-        for (int i = 0; i < DefaultValue.NumOfRowsShelf; i++) {
-            for (int j = 0; j < DefaultValue.NumOfColumnsShelf; j++) {
-                if ((i == 0 && j == 0) ||
-                        (i == DefaultValue.NumOfRowsShelf - 1 && j == DefaultValue.NumOfColumnsShelf - 1) ||
-                        (i == 0 && j == DefaultValue.NumOfColumnsShelf - 1))
-                    test.setSingleTile(new Tile(TileType.CAT), i, j);
-                else
-                    test.setSingleTile(new Tile(TileType.randomTileCAT()), i, j);
-            }
-        }
-        assertFalse(model.get(1).verify(test));
+    @DisplayName("Test vertexes #1")
+    public void testVertexes1() {
+        String[][] matrix = {
+                {"C","","","","C"},
+                {"","","","",""},
+                {"","","","",""},
+                {"","","","",""},
+                {"","","","",""},
+                {"C","","","","C"}
+        };
+        Shelf test = this.setShelf(matrix);
+        assertTrue(card.verify(test));
     }
 
     @Test
-    @DisplayName("Test with empty shelf")
-    public void testEmptyShelf() {
-        Shelf test = new Shelf();
-        for (int i = 0; i < DefaultValue.NumOfRowsShelf; i++) {
-            for (int j = 0; j < DefaultValue.NumOfColumnsShelf; j++) {
-                test.setSingleTile(new Tile(TileType.NOT_USED), i, j);
-            }
-        }
-        assertFalse(model.get(1).verify(test));
+    @DisplayName("Test vertexes #2")
+    public void testVertexes2() {
+        String[][] matrix = {
+                {"A","","","","C"},
+                {"","","","",""},
+                {"","","","",""},
+                {"","","","",""},
+                {"","","","",""},
+                {"C","","","","C"}
+        };
+        Shelf test = this.setShelf(matrix);
+        assertFalse(card.verify(test));
     }
+
+    @Test
+    @DisplayName("Test vertexes #3")
+    public void testVertexes3() {
+        String[][] matrix = {
+                {"C","C","","","A"},
+                {"C","C","","","A"},
+                {"C","C","","","A"},
+                {"C","C","","","A"},
+                {"C","C","","","A"},
+                {"C","C","A","A","A"}
+        };
+        Shelf test = this.setShelf(matrix);
+        assertFalse(card.verify(test));
+    }
+    @Test
+    @DisplayName("Test vertexes #4")
+    public void testVertexes4() {
+        String[][] matrix = {
+                {"A","A","A","T","A"},
+                {"A","","","","T"},
+                {"A","","","","T"},
+                {"A","","","","T"},
+                {"A","","","","T"},
+                {"A","A","A","T","A"}
+        };
+        Shelf test = this.setShelf(matrix);
+        assertTrue(card.verify(test));
+    }
+    @Test
+    @DisplayName("Test vertexes #5")
+    public void testVertexes5() {
+        String[][] matrix = {
+                {"A","","","","T"},
+                {"A","","","","A"},
+                {"A","","","","A"},
+                {"A","","","","A"},
+                {"A","","","","A"},
+                {"A","","","","A"}
+        };
+        Shelf test = this.setShelf(matrix);
+        assertFalse(card.verify(test));
+    }
+    @Test
+    @DisplayName("Test vertexes #6")
+    public void testVertexes6() {
+        String[][] matrix = {
+                {"A","A","A","A","A"},
+                {"A","A","A","A","A"},
+                {"A","A","A","A","A"},
+                {"A","A","A","A","A"},
+                {"A","A","A","A","A"},
+                {"A","A","A","A","A"}
+        };
+        Shelf test = this.setShelf(matrix);
+        assertTrue(card.verify(test));
+    }
+    @Test
+    @DisplayName("Test vertexes #7")
+    public void testVertexes7() {
+        String[][] matrix = {
+                {"T","A","A","A","A"},
+                {"A","A","A","A","A"},
+                {"A","A","A","A","A"},
+                {"A","A","A","A","A"},
+                {"A","A","A","A","A"},
+                {"A","A","A","A","A"}
+        };
+        Shelf test = this.setShelf(matrix);
+        assertFalse(card.verify(test));
+    }
+    @Test
+    @DisplayName("Test vertexes #8")
+    public void testVertexes8() {
+        String[][] matrix = {
+                {"T","A","A","A","T"},
+                {"A","A","A","A","A"},
+                {"A","A","A","A","A"},
+                {"A","A","A","A","A"},
+                {"A","A","A","A","A"},
+                {"A","A","A","A","T"}
+        };
+        Shelf test = this.setShelf(matrix);
+        assertFalse(card.verify(test));
+    }
+    @Test
+    @DisplayName("Test vertexes #9")
+    public void testVertexes9() {
+        String[][] matrix = {
+                {"T","A","A","A","T"},
+                {"A","A","A","A","A"},
+                {"A","A","T","T","A"},
+                {"A","T","T","A","A"},
+                {"A","A","T","A","A"},
+                {"T","A","A","A","T"}
+        };
+        Shelf test = this.setShelf(matrix);
+        assertTrue(card.verify(test));
+    }
+
+    @Test
+    @DisplayName("Test vertexes #11")
+    public void testVertexes10() {
+        String[][] matrix = {
+                {"C","A","A","A","C"},
+                {"A","A","A","A","A"},
+                {"A","A","T","T","A"},
+                {"A","T","T","A","C"},
+                {"C","A","T","C","A"},
+                {"A","C","A","A","T"}
+        };
+        Shelf test = this.setShelf(matrix);
+        assertFalse(card.verify(test));
+    }
+
 }
