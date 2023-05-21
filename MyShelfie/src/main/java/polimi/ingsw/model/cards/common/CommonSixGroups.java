0f84ed26f5aa8d58debc78a2a78a6df633ef7800
Francesco Spangaro <francescospangaro@gmail.com>
2023-05-21 16:38:16 +0200

    Corrected six tiles algorithm

--- MyShelfie/src/main/java/polimi/ingsw/model/cards/common/CommonSixGroups.java
+++ MyShelfie/src/main/java/polimi/ingsw/model/cards/common/CommonSixGroups.java
@@ -24,13 +24,17 @@ public class CommonSixGroups extends CommonMethods {
                     if (!temp.get(r, c).isSameType(TileType.NOT_USED)) {
                         if (r != DefaultValue.NumOfRowsShelf - 1) {
                             if (temp.get(r, c).isSameType(temp.get(r + 1, c).getType())) {
-                                sum++;
+                                temp.setSingleTile(new Tile(TileType.NOT_USED), r, c);
                                 temp.setSingleTile(new Tile(TileType.NOT_USED), r + 1, c);
+                                sum++;
                             }
                         } if (c != DefaultValue.NumOfColumnsShelf - 1) {
-                            if (temp.get(r, c).isSameType(temp.get(r, c + 1).getType())) {
-                                sum++;
+                            //need to check if the tile is different from a non-used one, in case the
+                            // row check passes and sets the tile to not used before
+                            if (temp.get(r, c).isSameType(temp.get(r, c + 1).getType()) && !temp.get(r, c).isSameType(TileType.NOT_USED)) {
+                                temp.setSingleTile(new Tile(TileType.NOT_USED), r, c);
                                 temp.setSingleTile(new Tile(TileType.NOT_USED), r, c + 1);
+                                sum++;
                             }
                         }
                         temp.setSingleTile(new Tile(TileType.NOT_USED), r, c);
