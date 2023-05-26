30d83463a5d558a9e742772129164706685f2f5a
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-05-26 12:25:29 +0200

    Create GameFlowTest.java

---
+++ MyShelfie/src/test/java/polimi/ingsw/view/userView/GameFlowTest.java
@@ -0,0 +1,71 @@
+package polimi.ingsw.view.userView;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import polimi.ingsw.model.DefaultValue;
+import polimi.ingsw.view.networking.RMI.RMIServer;
+import polimi.ingsw.view.networking.socket.server.SocketWelcome;
+import polimi.ingsw.view.userView.gui.GUIApplication;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class GameFlowTest {
+
+    private static RMIServer server;
+    private GameFlow gameFlow;
+
+    @BeforeEach
+    public void setUp() throws IOException {
+
+        System.setProperty("java.rmi.server.hostname", DefaultValue.Remote_ip);
+
+        server = new RMIServer();
+        server = RMIServer.bind();
+
+        SocketWelcome serverSOCKET = new SocketWelcome();
+        serverSOCKET.start(DefaultValue.Default_port_Socket);
+
+
+    }
+
+    @Test
+    public void testConstructorWithConnectionSelection_Socket() {
+        ConnectionSelection connectionSelection = ConnectionSelection.SOCKET;
+        gameFlow = new GameFlow(connectionSelection);
+
+        assertNotNull(gameFlow);
+        // Assert any other conditions or behaviors you expect from the constructor
+    }
+
+    @Test
+    public void testConstructorWithConnectionSelection_RMI() {
+        ConnectionSelection connectionSelection = ConnectionSelection.RMI;
+        gameFlow = new GameFlow(connectionSelection);
+
+        assertNotNull(gameFlow);
+        // Assert any other conditions or behaviors you expect from the constructor
+    }
+
+    @Test
+    public void testConstructorWithGUIApplicationAndConnectionSelection_Socket() {
+        GUIApplication guiApplication = new GUIApplication();
+        ConnectionSelection connectionSelection = ConnectionSelection.SOCKET;
+        gameFlow = new GameFlow(guiApplication, connectionSelection);
+
+        assertNotNull(gameFlow);
+        // Assert any other conditions or behaviors you expect from the constructor
+    }
+
+    @Test
+    public void testConstructorWithGUIApplicationAndConnectionSelection_RMI() {
+        GUIApplication guiApplication = new GUIApplication();
+        ConnectionSelection connectionSelection = ConnectionSelection.RMI;
+        gameFlow = new GameFlow(guiApplication, connectionSelection);
+
+        assertNotNull(gameFlow);
+        // Assert any other conditions or behaviors you expect from the constructor
+    }
+
+}
