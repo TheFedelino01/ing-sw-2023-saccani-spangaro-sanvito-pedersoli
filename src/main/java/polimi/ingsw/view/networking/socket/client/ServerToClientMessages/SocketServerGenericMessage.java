d731bd4fb3a2a8f669a7ab60fed985ae08a1d995
Luchino <lu.sanvito@gmail.com>
2023-06-01 17:58:25 +0200

    Update ServerToClientMessages #2

--- src/main/java/polimi/ingsw/view/networking/socket/client/ServerToClientMessages/SocketServerGenericMessage.java
+++ src/main/java/polimi/ingsw/view/networking/socket/client/ServerToClientMessages/SocketServerGenericMessage.java
@@ -5,8 +5,18 @@ import polimi.ingsw.listener.GameListener;
 import java.io.IOException;
 import java.io.Serializable;
 
+/**
+ * SocketServerGenericMessage class.
+ * An abstract class that represents a generic message to be sent from the server to the client.
+ */
 public abstract class SocketServerGenericMessage implements Serializable {
 
+    /**
+     * Executes the corresponding action for the message.
+     * @param lis the game listener
+     * @throws IOException if there is an IO exception
+     * @throws InterruptedException if the execution is interrupted
+     */
     public abstract void execute(GameListener lis) throws IOException, InterruptedException;
 
 }
--- src/main/java/polimi/ingsw/view/networking/socket/client/ServerToClientMessages/msgPositionedTile.java
+++ src/main/java/polimi/ingsw/view/networking/socket/client/ServerToClientMessages/msgPositionedTile.java
@@ -6,17 +6,33 @@ import polimi.ingsw.model.gameModelImmutable.GameModelImmutable;
 
 import java.rmi.RemoteException;
 
+/**
+ * msgPositionedTile class.
+ * Extends SocketServerGenericMessage and is used to send a message to the client
+ * indicating that a tile has been positioned on a shelf.
+ */
 public class msgPositionedTile extends SocketServerGenericMessage {
     private GameModelImmutable gamemodel;
     private TileType type;
     private int column;
 
+    /**
+     * Constructor of the class.
+     * @param gamemodel the immutable game model
+     * @param type the type of the positioned tile
+     * @param column the column where the tile is positioned
+     */
     public msgPositionedTile(GameModelImmutable gamemodel, TileType type, int column) {
         this.gamemodel = gamemodel;
         this.type = type;
         this.column = column;
     }
 
+    /**
+     * Method to execute the corresponding action for the message.
+     * @param lis the game listener
+     * @throws RemoteException if there is a remote exception
+     */
     @Override
     public void execute(GameListener lis) throws RemoteException {
         lis.positionedTile(gamemodel, type, column);
