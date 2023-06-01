07946051e35e02664132d7154a4cace2968fbe44
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-06-01 16:00:33 +0200

    Update GameListenersHandlerSocket.java

--- src/main/java/polimi/ingsw/view/networking/socket/server/GameListenersHandlerSocket.java
+++ src/main/java/polimi/ingsw/view/networking/socket/server/GameListenersHandlerSocket.java
@@ -223,7 +223,7 @@ public class GameListenersHandlerSocket implements GameListener, Serializable {
     /**
      * This method is used to write on the ObjectOutputStream that a Tile has been grabbed
      * @param gamemodel is the game model {@link GameModelImmutable}
-     * @throws RemoteException 
+     * @throws RemoteException if the connection fails
      */
     @Override
     public void grabbedTile(GameModelImmutable gamemodel) throws RemoteException {
