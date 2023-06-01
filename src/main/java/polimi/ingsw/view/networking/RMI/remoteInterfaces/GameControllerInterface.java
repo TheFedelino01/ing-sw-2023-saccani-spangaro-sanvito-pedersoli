e0fbfa928f181edb4faa52a62e82d2b0eeff9383
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-06-01 11:53:35 +0200

    Update GameControllerInterface.java

--- src/main/java/polimi/ingsw/view/networking/RMI/remoteInterfaces/GameControllerInterface.java
+++ src/main/java/polimi/ingsw/view/networking/RMI/remoteInterfaces/GameControllerInterface.java
@@ -41,18 +41,56 @@ public interface GameControllerInterface extends Remote {
      */
     void positionTileOnShelf(String p, int column, TileType type) throws RemoteException, GameEndedException;
 
-    
+    /**
+     * This method checks if it's the turn of the player
+     * @param nick the nickname of the player
+     * @return true if it's the turn of the player
+     * @throws RemoteException if the connection fails
+     */
     boolean isThisMyTurn(String nick) throws RemoteException;
 
+    /**
+     * This method disconnect a player and remove him from the GameListener list{@link GameListener}
+     * @param nick the nickname of the player
+     * @param lisOfClient the GameListener of the player {@link GameListener}
+     * @throws RemoteException if the connection fails
+     */
     void disconnectPlayer(String nick, GameListener lisOfClient) throws RemoteException;
 
+    /**
+     * This method is used to check if the client is connected, every x seconds the server send a ping to the client
+     * @param nick the nickname of the player
+     * @param me the GameListener of the player {@link GameListener}
+     * @throws RemoteException if the connection fails
+     */
     void heartbeat(String nick, GameListener me) throws RemoteException;
 
+    /**
+     * This method sends a message
+     * @param msg the message to send {@link Message}
+     * @throws RemoteException if the connection fails
+     */
     void sentMessage(Message msg) throws RemoteException;
 
+    /**
+     * This method return the id of the game
+     * @return the id of the game
+     * @throws RemoteException if the connection fails
+     */
     int getGameId() throws RemoteException;
 
+    /**
+     * This method return the number of the online players
+     * @return the number of the online players
+     * @throws RemoteException if the connection fails
+     */
     int getNumOnlinePlayers() throws RemoteException;
 
+    /**
+     * This method remove a player from the GameListener list {@link GameListener} and from the game
+     * @param lis the GameListener of the player {@link GameListener}
+     * @param nick the nickname of the player
+     * @throws RemoteException if the connection fails
+     */
     void leave(GameListener lis, String nick) throws RemoteException;
 }
