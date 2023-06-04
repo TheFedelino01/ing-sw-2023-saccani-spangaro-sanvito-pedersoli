9528d75b3f988a486fe41a8f7c04a8ac52095b2d
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-06-04 10:26:03 +0200

    Update CommonCardAlreadyInException.java

--- src/main/java/polimi/ingsw/model/exceptions/CommonCardAlreadyInException.java
+++ src/main/java/polimi/ingsw/model/exceptions/CommonCardAlreadyInException.java
@@ -1,5 +1,8 @@
 package polimi.ingsw.model.exceptions;
 
+/**
+ * This exception is thrown when a CommonCard is already in the game
+ */
 public class CommonCardAlreadyInException extends Exception {
     public CommonCardAlreadyInException() {
         super();
