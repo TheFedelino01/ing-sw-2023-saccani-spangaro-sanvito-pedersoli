174987b533026c596ea5ea4b6a81e05c17535898
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-06-04 10:27:09 +0200

    Update IndexPlayerOutOfBoundException.java

--- src/main/java/polimi/ingsw/model/exceptions/IndexPlayerOutOfBoundException.java
+++ src/main/java/polimi/ingsw/model/exceptions/IndexPlayerOutOfBoundException.java
@@ -1,6 +1,8 @@
 package polimi.ingsw.model.exceptions;
 
-
+/**
+ * This exception is thrown when the index of the player is out of bound
+ */
 public class IndexPlayerOutOfBoundException extends RuntimeException {
     public IndexPlayerOutOfBoundException() {
         super();
