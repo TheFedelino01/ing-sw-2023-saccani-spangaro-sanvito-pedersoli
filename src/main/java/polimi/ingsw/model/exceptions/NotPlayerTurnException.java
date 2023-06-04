07804052475b991ed7e50446051924b3f3d05c7a
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-06-04 10:27:46 +0200

    Update NotPlayerTurnException.java

--- src/main/java/polimi/ingsw/model/exceptions/NotPlayerTurnException.java
+++ src/main/java/polimi/ingsw/model/exceptions/NotPlayerTurnException.java
@@ -1,5 +1,8 @@
 package polimi.ingsw.model.exceptions;
 
+/**
+ * This exception is thrown when a player that is not playing tries to perform an action
+ */
 public class NotPlayerTurnException extends RuntimeException {
     public NotPlayerTurnException() {
         super();
