791164b5333cf7ddddb6e7af967a69ed63961578
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-06-04 10:27:17 +0200

    Update MaxCommonCardsAddedException.java

--- src/main/java/polimi/ingsw/model/exceptions/MaxCommonCardsAddedException.java
+++ src/main/java/polimi/ingsw/model/exceptions/MaxCommonCardsAddedException.java
@@ -1,5 +1,8 @@
 package polimi.ingsw.model.exceptions;
 
+/**
+ * This exception is thrown when the maximum number of CommonCards is reached
+ */
 public class MaxCommonCardsAddedException extends Exception {
     public MaxCommonCardsAddedException() {
         super();
