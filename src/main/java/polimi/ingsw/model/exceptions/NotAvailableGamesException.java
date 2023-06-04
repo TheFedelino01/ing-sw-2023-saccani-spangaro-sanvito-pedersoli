fd118c66c9335ebee15b9be7fe73882075ed2786
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-06-04 10:27:31 +0200

    Update NotAvailableGamesException.java

--- src/main/java/polimi/ingsw/model/exceptions/NotAvailableGamesException.java
+++ src/main/java/polimi/ingsw/model/exceptions/NotAvailableGamesException.java
@@ -1,5 +1,8 @@
 package polimi.ingsw.model.exceptions;
 
+/**
+ * This exception is thrown when there are no available games
+ */
 public class NotAvailableGamesException extends Exception {
     public NotAvailableGamesException() {
         super();
