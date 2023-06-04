7bda10c31c687ac7106aca92d6f4ed920cf53b26
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-06-04 10:28:42 +0200

    Update SecretGoalAlreadyGivenException.java

--- src/main/java/polimi/ingsw/model/exceptions/SecretGoalAlreadyGivenException.java
+++ src/main/java/polimi/ingsw/model/exceptions/SecretGoalAlreadyGivenException.java
@@ -1,5 +1,8 @@
 package polimi.ingsw.model.exceptions;
 
+/**
+ * This exception is thrown when the secret goal is already given
+ */
 public class SecretGoalAlreadyGivenException extends Exception {
     public SecretGoalAlreadyGivenException() {
         super();
