5542a8c74ef6c8a7062a2ddfa90f6994c8db2846
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-06-04 10:26:27 +0200

    Update GameNotStartedException.java

--- src/main/java/polimi/ingsw/model/exceptions/GameNotStartedException.java
+++ src/main/java/polimi/ingsw/model/exceptions/GameNotStartedException.java
@@ -1,5 +1,8 @@
 package polimi.ingsw.model.exceptions;
 
+/**
+ * This exception is thrown when the game is not started
+ */
 public class GameNotStartedException extends RuntimeException {
     public GameNotStartedException() {
         super();
