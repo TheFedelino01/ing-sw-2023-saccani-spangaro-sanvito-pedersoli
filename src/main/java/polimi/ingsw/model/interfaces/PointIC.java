4137a68d3732624d18d131444a81cf8875289765
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-06-04 10:42:41 +0200

    Update PointIC.java

--- src/main/java/polimi/ingsw/model/interfaces/PointIC.java
+++ src/main/java/polimi/ingsw/model/interfaces/PointIC.java
@@ -2,9 +2,20 @@ package polimi.ingsw.model.interfaces;
 
 import polimi.ingsw.model.enumeration.CardType;
 
+/**
+ * This interface is implemented by Point
+ */
 public interface PointIC {
+    /**
+     * This method is used to get the point
+     * @return the point
+     */
     Integer getPoint();
 
+    /**
+     * This method is used to get the card type this point is referred to
+     * @return the card type this point is referred to {@link CardType}
+     */
     CardType getReferredTo();
 
 }
