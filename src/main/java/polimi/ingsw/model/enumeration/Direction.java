9c5ff66d82e0ffed6c22657b7c067ad6564b220b
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-06-04 10:24:56 +0200

    Update Direction.java

--- src/main/java/polimi/ingsw/model/enumeration/Direction.java
+++ src/main/java/polimi/ingsw/model/enumeration/Direction.java
@@ -1,10 +1,14 @@
 package polimi.ingsw.model.enumeration;
 
+/**
+ * This enum represents the directions
+ */
 public enum Direction {
     UP, DOWN, LEFT, RIGHT;
 
     public static Direction getDirection(String txt) {
         String ris = txt.toLowerCase();
+
 
         return switch (ris) {
             case "up", "u" -> Direction.UP;
