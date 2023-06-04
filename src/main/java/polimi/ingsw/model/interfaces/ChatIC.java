5935b58f9b9989553fe050d0d0af2651c97631f3
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-06-04 10:34:00 +0200

    Update ChatIC.java

--- src/main/java/polimi/ingsw/model/interfaces/ChatIC.java
+++ src/main/java/polimi/ingsw/model/interfaces/ChatIC.java
@@ -4,14 +4,39 @@ import polimi.ingsw.model.chat.Message;
 
 import java.util.List;
 
+/**
+ * This interface is implemented by Chat class
+ */
 public interface ChatIC {
+
+    /**
+     * This method adds a message to the chat
+     * @return the message added {@link Message}
+     */
     List<Message> getMsgs();
 
+    /**
+     * This method returns the last message
+     * @return the last message
+     */
     String getLast();
 
+    /**
+     * This method returns the last message
+     * @return the last message {@link Message}
+     */
     Message getLastMessage();
 
+    /**
+     * This method returns a string representation
+     * @return the string representation
+     */
     String toString();
 
+    /**
+     * This method returns a string representation of the private message
+     * @param privateMsgByNickname nickname of the player
+     * @return the string representation
+     */
     String toString(String privateMsgByNickname);
 }
