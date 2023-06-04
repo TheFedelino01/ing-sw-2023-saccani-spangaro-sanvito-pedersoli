3635d5d5db9e3921651275b1b4c49df2fb457521
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-06-04 10:27:37 +0200

    Update NotEmptyHandException.java

--- src/main/java/polimi/ingsw/model/exceptions/NotEmptyHandException.java
+++ src/main/java/polimi/ingsw/model/exceptions/NotEmptyHandException.java
@@ -1,5 +1,8 @@
 package polimi.ingsw.model.exceptions;
 
+/**
+ * This exception is thrown when the player's hand is not empty
+ */
 public class NotEmptyHandException extends RuntimeException {
     public NotEmptyHandException() {
         super();
