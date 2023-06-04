db09268c0d930f76ee6d171bab3125b4bc43cabe
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-06-04 10:28:27 +0200

    Update PositioningATileNotGrabbedException.java

--- src/main/java/polimi/ingsw/model/exceptions/PositioningATileNotGrabbedException.java
+++ src/main/java/polimi/ingsw/model/exceptions/PositioningATileNotGrabbedException.java
@@ -1,5 +1,8 @@
 package polimi.ingsw.model.exceptions;
 
+/**
+ * This exception is thrown when a player tries to position a tile that he has not grabbed
+ */
 public class PositioningATileNotGrabbedException extends RuntimeException {
     public PositioningATileNotGrabbedException() {
         super();
