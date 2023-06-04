8d5fee70e804fc362b59657574a90cf132a643fb
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-06-04 10:28:15 +0200

    Update PlayerAlreadyInException.java

--- src/main/java/polimi/ingsw/model/exceptions/PlayerAlreadyInException.java
+++ src/main/java/polimi/ingsw/model/exceptions/PlayerAlreadyInException.java
@@ -1,5 +1,8 @@
 package polimi.ingsw.model.exceptions;
 
+/**
+ * This exception is thrown when a player is already in the game
+ */
 public class PlayerAlreadyInException extends RuntimeException {
     public PlayerAlreadyInException() {
         super("Player already in exception");
