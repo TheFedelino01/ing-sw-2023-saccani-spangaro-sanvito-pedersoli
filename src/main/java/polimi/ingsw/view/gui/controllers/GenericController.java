4c2e1e182f651eb8d6b76481bb8eb79af0129feb
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-06-04 14:24:03 +0200

    Update GenericController.java

--- src/main/java/polimi/ingsw/view/gui/controllers/GenericController.java
+++ src/main/java/polimi/ingsw/view/gui/controllers/GenericController.java
@@ -2,13 +2,24 @@ package polimi.ingsw.view.gui.controllers;
 
 import polimi.ingsw.view.flow.utilities.inputReaderGUI;
 
+/**
+ * GenericController class.
+ */
 public abstract class GenericController {
     private inputReaderGUI inputReaderGUI;
 
+    /**
+     * Method to get the input reader GUI.
+     * @return the input reader GUI
+     */
     public inputReaderGUI getInputReaderGUI() {
         return inputReaderGUI;
     }
 
+    /**
+     * Method to set the input reader GUI.
+     * @param inputReaderGUI the input reader GUI
+     */
     public void setInputReaderGUI(polimi.ingsw.view.flow.utilities.inputReaderGUI inputReaderGUI) {
         this.inputReaderGUI = inputReaderGUI;
     }
