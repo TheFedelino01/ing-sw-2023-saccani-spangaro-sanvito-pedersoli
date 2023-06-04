fb38734203bd606fd6709df3749935f9d25d6615
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-06-04 10:36:01 +0200

    Update CommonCardIC.java

--- src/main/java/polimi/ingsw/model/interfaces/CommonCardIC.java
+++ src/main/java/polimi/ingsw/model/interfaces/CommonCardIC.java
@@ -7,18 +7,48 @@ import polimi.ingsw.model.enumeration.CardCommonType;
 
 import java.util.Queue;
 
-
+/**
+ * This interface is implemented by CardCommon
+ */
 public interface CommonCardIC {
 
+    /**
+     * This method is used to verify if the player's shelf matches the layoutToMatch
+     * @param toCheck player's shelf
+     * @return true if the player's shelf matches the layoutToMatch
+     */
     boolean verify(Shelf toCheck);
 
+    /**
+     * To string implementation
+     * @param i spacer
+     * @return string representation of the card
+     */
     String toString(int i);
 
+    /**
+     * This method is used to get the points of the CardCommon
+     * @return the points of the CardCommon
+     */
     Queue<PointIC> getPoints_IC();
 
+    /**
+     * This method is used to get the type of the CardCommon
+     * @return the type of the CardCommon
+     */
     CardCommonType getCommonType();
 
+    /**
+     * This method check if the type of the card is the same
+     * @param c card to check
+     * @return true if the type is the same
+     */
     boolean isSameType(Card c);
 
+    /**
+     * This method check if the card is the same
+     * @param c card to check
+     * @return true if the card is the same
+     */
     boolean equals(CommonCard c);
 }
