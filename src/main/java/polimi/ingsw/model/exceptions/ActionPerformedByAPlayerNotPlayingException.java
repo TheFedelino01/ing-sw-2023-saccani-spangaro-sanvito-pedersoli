e5aee25948ebac3696cb5d9be2b9199278bbbef1
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-06-04 10:25:48 +0200

    Update ActionPerformedByAPlayerNotPlayingException.java

--- src/main/java/polimi/ingsw/model/exceptions/ActionPerformedByAPlayerNotPlayingException.java
+++ src/main/java/polimi/ingsw/model/exceptions/ActionPerformedByAPlayerNotPlayingException.java
@@ -1,5 +1,8 @@
 package polimi.ingsw.model.exceptions;
 
+/**
+ * This exception is thrown when a player that is not playing tries to perform an action
+ */
 public class ActionPerformedByAPlayerNotPlayingException extends RuntimeException {
     public ActionPerformedByAPlayerNotPlayingException() {
         super();
