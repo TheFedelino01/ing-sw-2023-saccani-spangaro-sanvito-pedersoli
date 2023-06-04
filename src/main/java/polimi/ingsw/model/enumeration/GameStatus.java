409a1448fe06e06ffbc41cad992596d4d522decf
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-06-04 10:25:03 +0200

    Update GameStatus.java

--- src/main/java/polimi/ingsw/model/enumeration/GameStatus.java
+++ src/main/java/polimi/ingsw/model/enumeration/GameStatus.java
@@ -2,6 +2,9 @@ package polimi.ingsw.model.enumeration;
 
 import java.io.Serializable;
 
+/**
+ * This enum represents the game status
+ */
 public enum GameStatus implements Serializable {
     WAIT,
     RUNNING,
