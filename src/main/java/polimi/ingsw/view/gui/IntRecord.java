46db8d62af10cb1c5f63981c92e9ac9537cf1b9e
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-06-04 14:02:47 +0200

    Update IntRecord.java

--- src/main/java/polimi/ingsw/view/gui/IntRecord.java
+++ src/main/java/polimi/ingsw/view/gui/IntRecord.java
@@ -1,3 +1,8 @@
 package polimi.ingsw.view.gui;
 
+/**
+ * IntRecord class. This class is used to store a pair of integers which represent a point on a matrix.
+ * @param row the row
+ * @param col the column
+ */
 public record IntRecord(int row, int col) {}
