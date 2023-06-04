c96b756ee29e0ecec780ad502692789c5567805b
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-06-04 10:28:59 +0200

    Update TileGrabbedNotCorrectException.java

--- src/main/java/polimi/ingsw/model/exceptions/TileGrabbedNotCorrectException.java
+++ src/main/java/polimi/ingsw/model/exceptions/TileGrabbedNotCorrectException.java
@@ -1,5 +1,8 @@
 package polimi.ingsw.model.exceptions;
 
+/**
+ * This exception is thrown when the player tries to grab a tile that is not correct
+ */
 public class TileGrabbedNotCorrectException extends Exception {
     public TileGrabbedNotCorrectException() {
         super();
