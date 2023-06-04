e01179605c9fe8e1dc0946ec7894ff1765b13384
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-06-04 10:27:52 +0200

    Update NotReadyToRunException.java

--- src/main/java/polimi/ingsw/model/exceptions/NotReadyToRunException.java
+++ src/main/java/polimi/ingsw/model/exceptions/NotReadyToRunException.java
@@ -1,5 +1,8 @@
 package polimi.ingsw.model.exceptions;
 
+/**
+ * This exception is thrown when the player is not ready to run
+ */
 public class NotReadyToRunException extends RuntimeException {
     public NotReadyToRunException() {
         super();
