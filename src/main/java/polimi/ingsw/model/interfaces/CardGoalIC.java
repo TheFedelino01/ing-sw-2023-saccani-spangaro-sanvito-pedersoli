4756cd629a339f58b52e4ff79b4e88363632d1e6
Luca Pedersoli <luca.pedersoli@mail.polimi.it>
2023-06-04 10:31:53 +0200

    Update CardGoalIC.java

--- src/main/java/polimi/ingsw/model/interfaces/CardGoalIC.java
+++ src/main/java/polimi/ingsw/model/interfaces/CardGoalIC.java
@@ -7,17 +7,46 @@ import polimi.ingsw.model.enumeration.CardGoalType;
 
 import java.util.Map;
 
+/**
+ * This interface is implemented by CardGoal
+ */
 public interface CardGoalIC {
+    /**
+     * This method is used to check if the player's shelf matches the layoutToMatch
+     * @param toCheck player's shelf
+     * @return the points gained
+     */
     PointIC verify(Shelf toCheck);
 
+    /**
+     * This method is used to get the layout that the CardGoal need to match
+     */
     ShelfIC getLayoutToMatch_IC();
 
+    /**
+     * This method is used to get the legend of the CardGoal
+     * @return a map that contains the points gained for each number of match
+     */
     Map<Integer, Point> getLegendPoint();
 
+    /**
+     * This method is used to get the type of the CardGoal
+     * @return the type of the CardGoal
+     */
     CardGoalType getGoalType();
 
+    /**
+     * This method is used to set the type of the CardGoal
+     * @param c type of the CardGoal
+     * @return true if the type is the same
+     */
     boolean isSameType(Card c);
 
+    /**
+     * This method is used to get the string representation of the CardGoal
+     * @param col number of the column
+     * @return the string representation of the CardGoal
+     */
     String toString(int col);
 
 }
