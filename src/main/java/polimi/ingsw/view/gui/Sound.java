79bc0257b29c9c47cdc82df3b09f6ba128be8613
TheFedelino01 <federico.saccani01@gmail.com>
2023-06-09 21:59:43 +0200

    Fixed audio path for jar and exe

--- src/main/java/polimi/ingsw/view/gui/Sound.java
+++ src/main/java/polimi/ingsw/view/gui/Sound.java
@@ -6,7 +6,10 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.FloatControl;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Paths;
 
@@ -20,9 +23,28 @@ public class Sound {
                 public void run() {
                     try {
                         Clip clip = AudioSystem.getClip();
-                        URL res = getClass().getClassLoader().getResource("audio/" + soundname);
-                        File file = Paths.get(res.toURI()).toFile();
-                        AudioInputStream inputStream = AudioSystem.getAudioInputStream(file);
+
+
+
+                        InputStream in = Sound.class.getResourceAsStream("/audio/"+soundname);
+
+
+                        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+                        while ((bytesRead = in.read(buffer)) != -1) {
+                            outputStream.write(buffer, 0, bytesRead);
+                        }
+                        byte[] audioData = outputStream.toByteArray();
+
+                        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(audioData);
+
+
+                        AudioInputStream inputStream = AudioSystem.getAudioInputStream(byteArrayInputStream);
+
+
+
+
                         clip.open(inputStream);
                         FloatControl gainControl =
                                 (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
